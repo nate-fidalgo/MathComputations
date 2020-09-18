e176429e6995ef889db0d7fd6496c6a1d614f079
nate-fidalgo <71114853+nate-fidalgo@users.noreply.github.com>
2020-09-18 17:39:30 -0400

    Add files via upload
    GUI program to visualize computing a convex hull given any point set

---
+++ Point.java
@@ -0,0 +1,57 @@
+
+public class Point {
+
+private int x_coordinate ;
+private int y_coordinate ;
+
+
+public Point( int x , int y )
+{
+
+    x_coordinate = x ;
+    y_coordinate = y ;
+
+}
+
+
+
+public void set_x( int x )
+{
+
+  x_coordinate = x ;
+
+}
+
+
+
+public void set_y( int y )
+{
+
+  y_coordinate = y ;
+
+}
+
+
+
+public int get_x()
+{
+
+  return x_coordinate ;
+
+}
+
+
+public int get_y()
+{
+
+  return y_coordinate ;
+
+}
+
+
+
+
+
+
+
+}
